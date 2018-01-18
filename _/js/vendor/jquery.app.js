998bfdfcb6eda8836c1d2c57b11f1177de372017
Jason Chiang <Cojad@users.noreply.github.com>
2018-01-19 01:33:29 +0800

    v1.2 update new vocals fix menu glitch in offline mode

--- _/js/vendor/jquery.app.js
+++ _/js/vendor/jquery.app.js
@@ -35,16 +35,16 @@
         // hide any open menus and remove all other classes
         //$("ul", $element.parents("ul:first")).slideUp(350);
         $("a", $element.parents("ul:first")).removeClass("subdrop");
-        //$("#sidebar-menu .pull-right i").removeClass("md-remove").addClass("md-add");
+        $("#sidebar-menu .pull-right i").removeClass("md-remove").addClass("md-add");
 
         // open our new menu and add the open class
-        //$element.next("ul").show();
+        $element.next("ul").show();
         $element.addClass("subdrop");
-        //$(".pull-right i", $element.parents(".has_sub:last")).removeClass("md-add").addClass("md-remove");
-        //$(".pull-right i", $element.siblings("ul")).removeClass("md-remove").addClass("md-add");
+        $(".pull-right i", $element.parents(".has_sub:last")).removeClass("md-add").addClass("md-remove");
+        $(".pull-right i", $element.siblings("ul")).removeClass("md-remove").addClass("md-add");
       } else {
         if ($element.parent().hasClass('has_sub')) {
-          $element.removeClass('subdrop').next('ul').hide();
+          //$element.removeClass('subdrop').next('ul').hide();
         }
       }
 
--- h/monitoring.php
+++ h/monitoring.php
@@ -14,7 +14,7 @@ curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
 $raw=curl_exec($ch);
 $redirectURL = curl_getinfo($ch,CURLINFO_EFFECTIVE_URL );
 if(stripos($redirectURL,"/login")!==false){
-	header("location: /login");
+  header("location: /login");
 };
 if(empty($raw)){
   header("location: /_/");
@@ -86,6 +86,16 @@ $s[]='main-text">Total Pending Value</p>';
 $r[]='main-text">潛在總值</p>';
 $s[]='title="BTC trend last 24h"';
 $r[]='title="BTC 24小時趨勢"';
+$s[]='Last BTCUSD price from your Market';
+$r[]='來自您交易所的最新的美元匯率';
+$s[]='main-text">BTC USD Price</p>';
+$r[]='main-text">BTC USD 匯率</p>';
+$s[]='Average % Trend last hour of top 20 altcoins USD Market (CoinMarketCap)';
+$r[]='Average % Trend last hour of top 20 altcoins USD Market (CoinMarketCap)';
+$s[]='Average % Trend 24 hours of top 20 altcoins USD Market (CoinMarketCap)';
+$r[]='Average % Trend 24 hours of top 20 altcoins USD Market (CoinMarketCap)';
+$s[]='Average % Trend last 7 days of top 20 altcoins USD Market (CoinMarketCap)';
+$r[]='Average % Trend last 7 days of top 20 altcoins USD Market (CoinMarketCap)';
 $s[]='main-text">Trend USD last 1H</p>';
 $r[]='main-text">USD 一小時趨勢</p>';
 $s[]='main-text">Trend USD last 24H</p>';
@@ -104,36 +114,70 @@ $s[]='full-text">Target Value</span>';
 $r[]='full-text">目標價</span>';
 $s[]='full-text">Sold Value</span>';
 $r[]='full-text">賣價</span>';
+$s[]='The current value of your pair coins';
+$r[]='The current value of your pair coins';
+$s[]='The current value of your pending coins';
+$r[]='The current value of your pending coins';
+$s[]='The current value of your DCA coins';
+$r[]='The current value of your DCA coins';
 $s[]='short-text">CV</span>';
 $r[]='short-text">現值</span>';
+$s[]='The difference between Current Value and Bought Cost';
+$r[]='The difference between Current Value and Bought Cost';
+$s[]='The difference between Current Value and Total Cost';
+$r[]='The difference between Current Value and Total Cost';
+$s[]='The difference between Bought Cost and Sold Value';
+$r[]='The difference between Bought Cost and Sold Value';
+$s[]='The bought cost of your sold coins';
+$r[]='The bought cost of your sold coins';
 $s[]='short-text">BC</span>';
 $r[]='short-text">買價</span>';
+$s[]='The bought cost of your pair coins';
+$r[]='The bought cost of your pair coins';
 $s[]='short-text">TC</span>';
 $r[]='short-text">花費</span>';
+$s[]='The target value of your pending coins';
+$r[]='The target value of your pending coins';
 $s[]='short-text">TV</span>';
 $r[]='short-text">目標價</span>';
+$s[]='The sold value of your sold coins';
+$r[]='The sold value of your sold coins';
 $s[]='short-text">SV</span>';
 $r[]='short-text">賣價</span>';
-
+$s[]='Your profit from last week';
+$r[]='Your profit from last week';
 $s[]='main-text">Profit Last Week</p>';
 $r[]='main-text">上週收益</p>';
+$s[]='Your profit from last week';
+$r[]='Your profit from last week';
 $s[]='main-text">Profit Yesterday</p>';
 $r[]='main-text">昨日收益</p>';
+$s[]='Your profit from today';
+$r[]='Your profit from today';
 $s[]='main-text">Profit Today</p>';
 $r[]='main-text">今日收益</p>';
-
+$s[]='ProfitTrailer Version';
+$r[]='ProfitTrailer Version';
+$s[]='Pending Order Time';
+$r[]='Pending Order Time';
+$s[]='SOM: </label>';
+$r[]='出貨模式 SOM: </label>';
+$s[]='SOMO: </label>';
+$r[]='SOMO: </label>';
+$s[]='Sell Only Mode Override';
+$r[]='Sell Only Mode Override';
 $s[]='<!-- SETTINGS -->';
 $r[]='<!-- BOT -->
         <div id="tmplBot" class="hide">
           <div id="configurationContainer" class="row">
-						<div class="col-12 configuration-heading-container">
+            <div class="col-12 configuration-heading-container">
               <button type="button" style="cursor: pointer;color: #ffffff;" class="btn btn-primary btn-sm bot-on"><i class="fa fa-rocket" aria-hidden="true"></i> 啟動</button>&nbsp;&nbsp;&nbsp;
               <button type="button" style="cursor: pointer;color: #ffffff;" class="btn btn-danger btn-sm bot-off"><i class="fa fa-power-off" aria-hidden="true"></i> 停止</button>&nbsp;&nbsp;&nbsp;
               <button type="button" style="cursor: pointer;color: #ffffff;" class="btn btn-warning btn-sm bot-clear"><i class="fa fa-times" aria-hidden="true"></i> 清除記錄</button>
-						</div>
-	          <div class="bot-container editor-container col-12" style="">
-	          	<iframe id="logIFrame" src="" width="100%" height="100%"></iframe>
-	          </div>
+            </div>
+            <div class="bot-container editor-container col-12" style="">
+              <iframe id="logIFrame" src="" width="100%" height="100%"></iframe>
+            </div>
           </div>
         </div>
         <!-- end BOT -->
