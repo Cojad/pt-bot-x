317c2e49f7b97f5fd5a553b91f3fe14b9f6e64a4
Jason Chiang <Cojad@users.noreply.github.com>
2018-01-23 01:52:12 +0800

    fix version error

--- ptx/ptx.sh
+++ ptx/ptx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+pt_ver=1.2.6.11
+ptx_ver=2.2
 echo "Donload ProfitTrailer v1.2.6.11 from github!"
-wget https://github.com/taniman/profit-trailer/releases/download/v1.2.6.11/ProfitTrailer.zip -O ProfitTrailer.zip
+wget https://github.com/taniman/profit-trailer/releases/download/v$pt_ver/ProfitTrailer.zip -O ProfitTrailer.zip
 mkdir tmp
 echo "Unzip ProfitTrailer.jar from ProfitTrailer.zip"
 unzip -qoj ProfitTrailer.zip ProfitTrailer/ProfitTrailer.jar
@@ -14,13 +16,13 @@ rm js/script.js
 echo "remove temp files"
 rm -Rf tmp ProfitTrailer.jar
 echo "Download PT Bot X from github"
-wget https://github.com/Cojad/pt-bot-x/archive/v2.0.zip -O pt-box-x.zip
+wget https://github.com/Cojad/pt-bot-x/archive/v$ptx_ver.zip -O pt-box-x.zip
 echo "Unzip pt-box-x.zip"
 unzip pt-box-x.zip
 echo "Move file to current dir"
-cp -R pt-bot-x-2.2/* .
+cp -R pt-bot-x-$ptx_ver/* .
 echo "remove temp files"
-rm -Rf pt-bot-x-2.2
+rm -Rf pt-bot-x-$ptx_ver
 echo "create config.php with PT Bot Url to $1"
 echo -e "<?php \$url=\"$1\";?>" >> config.php;
 echo "All done! you can now use PT Bot X by Cojad"
