1559afa850e805b61189749d6a27dbbd6a7fcb15
Jason Chiang <Cojad@users.noreply.github.com>
2018-01-27 15:26:33 +0800

    update script for compatible issue

--- ptx/ptx.sh
+++ ptx/ptx.sh
@@ -9,18 +9,19 @@ unzip -qoj ProfitTrailer.zip ProfitTrailer/ProfitTrailer.jar
 echo "Unzip static files from ProfitTrailer.jar"
 unzip -q ProfitTrailer.jar -d tmp/
 echo "move files to current folder"
-cp -ruv tmp/BOOT-INF/classes/static/* .
-cp -ruv tmp/BOOT-INF/classes/templates .
+rm -Rf static templates ptx index.php config.php
+cp -r tmp/BOOT-INF/classes/static/* .
+cp -r tmp/BOOT-INF/classes/templates .
 touch css/xcustom.css
 mv js/script.js js/scriptX.js
 echo "remove temp files"
-rm -Rf tmp ProfitTrailer.jar
+rm -Rf tmp ProfitTrailer.jar ProfitTrailer.zip
 echo "Download PT Bot X from github"
 wget https://github.com/Cojad/pt-bot-x/archive/v$ptx_ver.zip -O pt-box-x.zip
 echo "Unzip pt-box-x.zip"
 unzip pt-box-x.zip
 echo "Move file to current dir"
-cp -ur pt-bot-x-$ptx_ver/* .
+cp -r pt-bot-x-$ptx_ver/* .
 echo "remove temp files"
-rm -Rf pt-bot-x-$ptx_ver
+rm -Rf pt-bot-x-$ptx_ver pt-box-x.zip
 echo "All done! Edit config.php to match your vps settings\n"
